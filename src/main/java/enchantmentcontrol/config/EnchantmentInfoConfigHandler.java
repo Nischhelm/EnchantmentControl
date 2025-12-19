a7bd217c6ee52f53b474ff95f54b06cee8bc1e92
nischi <nischhelm@gmail.com>
2025-12-19 12:26:45 +0100

    ai made it gson

--- src/main/java/enchantmentcontrol/config/EnchantmentInfoConfigHandler.java
+++ src/main/java/enchantmentcontrol/config/EnchantmentInfoConfigHandler.java
@@ -1,6 +1,7 @@
 package enchantmentcontrol.config;
 
-import com.google.gson.stream.JsonReader;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonToken;
 import enchantmentcontrol.EnchantmentControl;
 import enchantmentcontrol.util.EnchantmentInfo;
@@ -26,163 +27,140 @@ public class EnchantmentInfoConfigHandler {
         //create EnchantmentInfo from json cfg files, wish i had Meldexun Forge Config Extension
 
         try {
-            List<EnchantmentInfo> infos = readJsonStream(Files.newInputStream(new File(ENCH_CFG_PATH).toPath()));
+            List<EnchantmentInfo> infos = readWithGson(Files.newInputStream(new File(ENCH_CFG_PATH).toPath()));
         } catch (Exception e){
             EnchantmentControl.LOGGER.warn("Reading enchantment configs failed!");
             e.printStackTrace(System.out);
         }
     }
 
-    private static List<EnchantmentInfo> readJsonStream(InputStream in) throws IOException {
-        try (JsonReader reader = new JsonReader(new InputStreamReader(in, StandardCharsets.UTF_8))) {
-            return readEnchantmentInfosArray(reader);
+    private static List<EnchantmentInfo> readWithGson(InputStream in) throws IOException {
+        try (Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(EnchantmentInfo.class, new EnchantmentInfoDeserializer())
+                    .create();
+            java.lang.reflect.Type listType = new TypeToken<List<EnchantmentInfo>>(){}.getType();
+            List<EnchantmentInfo> infos = gson.fromJson(reader, listType);
+            return infos == null ? new ArrayList<>() : infos;
         }
     }
 
-    private static List<EnchantmentInfo> readEnchantmentInfosArray(JsonReader reader) throws IOException {
-        List<EnchantmentInfo> EnchantmentInfos = new ArrayList<>();
+    private static class EnchantmentInfoDeserializer implements JsonDeserializer<EnchantmentInfo> {
+        @Override
+        public EnchantmentInfo deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            if (!json.isJsonObject()) return null;
+            JsonObject o = json.getAsJsonObject();
 
-        reader.beginArray();
-        while (reader.hasNext()) {
-            EnchantmentInfos.add(readEnchantmentInfo(reader));
-        }
-        reader.endArray();
-        return EnchantmentInfos;
-    }
+            String id = getAsString(o, "id");
+            if (id == null) return null;
 
-    private static EnchantmentInfo readEnchantmentInfo(JsonReader reader) throws IOException {
-        String id = null;
-        Enchantment.Rarity rarity = null;
-        Boolean isTreasure = null;
-        Boolean isCurse = null;
-        Boolean isAllowedOnBooks = null;
-        Integer minLvl = null;
-        Integer maxLvl = null;
+            EnchantmentInfo info = new EnchantmentInfo(id);
 
-        Boolean custom_evaluations = null;
-        String min_ench_eval = null;
-        String max_ench_eval = null;
+            // rarity
+            String rarityStr = getAsString(o, "rarity");
+            if (rarityStr != null) info.setRarity(Enchantment.Rarity.valueOf(rarityStr));
 
-        Consumer<EnchantmentInfo> enchantabilitySetter = null;
+            // booleans (with legacy aliases)
+            Boolean isTreasure = getAsBoolean(o, "isTreasure");
+            if (isTreasure == null) isTreasure = getAsBoolean(o, "treasure");
+            if (isTreasure != null) info.setTreasure(isTreasure);
 
-        List<String> types = null;
-        List<String> typesAnvil = null;
-        List<EntityEquipmentSlot> slots = null;
+            Boolean isCurse = getAsBoolean(o, "isCurse");
+            if (isCurse == null) isCurse = getAsBoolean(o, "curse");
+            if (isCurse != null) info.setCurse(isCurse);
 
-        String displayColor = null;
+            Boolean allowedOnBooks = getAsBoolean(o, "isAllowedOnBooks");
+            if (allowedOnBooks == null) allowedOnBooks = getAsBoolean(o, "allowed_on_books");
+            if (allowedOnBooks != null) info.setAllowedOnBooks(allowedOnBooks);
 
-        Boolean doublePrice = null;
+            // levels (with legacy aliases)
+            Integer minLvl = getAsInt(o, "minLvl");
+            if (minLvl == null) minLvl = getAsInt(o, "min_lvl");
+            if (minLvl != null) info.setMinLvl(minLvl);
 
-        //TODO: a separate config file for just the descriptions
+            Integer maxLvl = getAsInt(o, "maxLvl");
+            if (maxLvl == null) maxLvl = getAsInt(o, "max_lvl");
+            if (maxLvl != null) info.setMaxLvl(maxLvl);
 
-        reader.beginObject();
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            switch (name){
-                case "id": id = reader.nextString(); break;
-                case "rarity": rarity = Enchantment.Rarity.valueOf(reader.nextString()); break;
-                case "isTreasure": case "treasure": isTreasure = reader.nextBoolean(); break;
-                case "isCurse": case "curse": isCurse = reader.nextBoolean(); break;
-                case "isAllowedOnBooks": case "allowed_on_books": isAllowedOnBooks = reader.nextBoolean(); break;
-                case "minLvl": case "min_lvl": minLvl = reader.nextInt(); break;
-                case "maxLvl": case "max_lvl": maxLvl = reader.nextInt(); break;
-                case "enchantability": enchantabilitySetter = readEnchantability(reader); break;
-                case "slots": case "equipment_slots": slots = readStringArray(reader).stream().map(EntityEquipmentSlot::valueOf).collect(Collectors.toList()); break;
-                case "types": if(reader.peek() != JsonToken.NULL) types = readStringArray(reader); break;
-                case "typesAnvil": if(reader.peek() != JsonToken.NULL) typesAnvil = readStringArray(reader); break;
-                case "displayColor": displayColor = reader.nextString(); break;
-                case "doublePrice": case "double_price": doublePrice = reader.nextBoolean(); break;
-                // --- only old enchcontrol ---
-                case "type": types = Collections.singletonList(reader.nextString()); break;
-                case "custom_evaluations": custom_evaluations = reader.nextBoolean(); break;
-                case "min_ench_eval": min_ench_eval = reader.nextString(); break;
-                case "max_ench_eval": max_ench_eval = reader.nextString(); break;
-                /*
-                Ignoring the following old Enchantments Control values
-                    name <- unlocalised name, idk why this would ever need to be changed
-                    enabled <- handled via registration blacklist
-                    hide_on_item <- idk if i like it, might implement later
-                    hide_on_book <- same
-                    incompat_mode <- idk why theres a mode. mode == 1 seems to fully disable all incompats
-                    incompatible <- handled via config groups
-                    applicability_mode <- i didnt understand this system
-                    items_list_mode <- same
-                    items_list <- same
-                    description <- handled in a separate config TODO
-                 */
-                default: {
-                    reader.skipValue();
-                    //EnchantmentControl.LOGGER.warn("Skipping entry with name {}", name);
-                    break;
+            // enchantability
+            if (o.has("enchantability") && o.get("enchantability").isJsonObject()) {
+                JsonObject enchObj = o.getAsJsonObject("enchantability");
+                Integer minEnch = getAsInt(enchObj, "minEnch");
+                Integer lvlSpan = getAsInt(enchObj, "lvlSpan");
+                Integer range = getAsInt(enchObj, "range");
+                String modeStr = getAsString(enchObj, "maxEnchMode");
+                MaxEnchantabilityMode mode = modeStr == null ? null : MaxEnchantabilityMode.valueOf(modeStr);
+                if (minEnch != null && range != null) {
+                    info.setEnchantabilities(minEnch, lvlSpan == null ? 0 : lvlSpan, range, mode);
                 }
             }
+
+            // slots (with legacy alias)
+            List<EntityEquipmentSlot> slots = readEnumList(o, "slots", EntityEquipmentSlot.class);
+            if (slots == null) slots = readEnumList(o, "equipment_slots", EntityEquipmentSlot.class);
+            if (slots != null) info.setSlots(slots);
+
+            // types
+            List<String> types = readStringList(o, "types");
+            if (types == null && o.has("type") && o.get("type").isJsonPrimitive()) {
+                types = Collections.singletonList(o.get("type").getAsString());
+            }
+            if (types != null) info.setEnchTableTypes(new HashSet<>(types));
+
+            List<String> typesAnvil = readStringList(o, "typesAnvil");
+            if (typesAnvil != null) info.setAnvilTypes(new HashSet<>(typesAnvil));
+
+            // displayColor
+            String colorStr = getAsString(o, "displayColor");
+            if (colorStr != null) info.setTextDisplayColor(TextFormatting.valueOf(colorStr));
+
+            // doublePrice (with legacy alias)
+            Boolean doublePrice = getAsBoolean(o, "doublePrice");
+            if (doublePrice == null) doublePrice = getAsBoolean(o, "double_price");
+            if (doublePrice != null) info.setDoublePrice(doublePrice);
+
+            // legacy enchantments control support
+            Boolean legacyCustom = getAsBoolean(o, "custom_evaluations");
+            String legacyMin = getAsString(o, "min_ench_eval");
+            String legacyMax = getAsString(o, "max_ench_eval");
+            if (Boolean.TRUE.equals(legacyCustom) && legacyMin != null && legacyMax != null) {
+                parseLegacyEnchantmentsControlConfig(info, legacyMin, legacyMax);
+            }
+
+            return info;
         }
-        reader.endObject();
 
-        if(id == null) return null;
-
-        EnchantmentInfo info = new EnchantmentInfo(id);
-        if(rarity != null) info.setRarity(rarity);
-        if(isTreasure != null) info.setTreasure(isTreasure);
-        if(isCurse != null) info.setCurse(isCurse);
-        if(isAllowedOnBooks != null) info.setAllowedOnBooks(isAllowedOnBooks);
-        if(minLvl != null) info.setMinLvl(minLvl);
-        if(maxLvl != null) info.setMaxLvl(maxLvl);
-        if(enchantabilitySetter != null) enchantabilitySetter.accept(info);
-        if(slots != null) info.setSlots(slots);
-        if(types != null) info.setEnchTableTypes(new HashSet<>(types));
-        if(typesAnvil != null) info.setAnvilTypes(new HashSet<>(typesAnvil));
-        if(displayColor != null) info.setTextDisplayColor(TextFormatting.valueOf(displayColor));
-        if(doublePrice != null) info.setDoublePrice(doublePrice);
-
-        if(custom_evaluations != null && custom_evaluations && min_ench_eval != null && max_ench_eval != null)
-            parseLegacyEnchantmentsControlConfig(info, min_ench_eval, max_ench_eval);
-
-        return new EnchantmentInfo(id);
-    }
-
-    private static List<String> readStringArray(JsonReader reader) throws IOException {
-        List<String> strings = new ArrayList<>();
-
-        reader.beginArray();
-        while (reader.hasNext()) {
-            strings.add(reader.nextString());
+        private static String getAsString(JsonObject o, String key) {
+            return o.has(key) && !o.get(key).isJsonNull() ? o.get(key).getAsString() : null;
         }
-        reader.endArray();
-        return strings;
-    }
-
-    public static Consumer<EnchantmentInfo> readEnchantability(JsonReader reader) throws IOException {
-        Integer minEnch = null;
-        Integer lvlSpan = null;
-        Integer range = null;
-        MaxEnchantabilityMode maxEnchMode = null;
-
-        reader.beginObject();
-        while (reader.hasNext()) {
-            String name = reader.nextName();
-            switch (name){
-                case "minEnch": minEnch = reader.nextInt(); break;
-                case "lvlSpan": lvlSpan = reader.nextInt(); break;
-                case "range": range = reader.nextInt(); break;
-                case "maxEnchMode": maxEnchMode = MaxEnchantabilityMode.valueOf(reader.nextString()); break;
-                default: {
-                    reader.skipValue();
-                    //EnchantmentControl.LOGGER.warn("Skipping entry with name {}", name);
-                    break;
+        private static Boolean getAsBoolean(JsonObject o, String key) {
+            return o.has(key) && !o.get(key).isJsonNull() ? o.get(key).getAsBoolean() : null;
+        }
+        private static Integer getAsInt(JsonObject o, String key) {
+            return o.has(key) && !o.get(key).isJsonNull() ? o.get(key).getAsInt() : null;
+        }
+        private static List<String> readStringList(JsonObject o, String key) {
+            if (!o.has(key) || o.get(key).isJsonNull()) return null;
+            JsonElement e = o.get(key);
+            if (!e.isJsonArray()) return null;
+            List<String> out = new ArrayList<>();
+            for (JsonElement je : e.getAsJsonArray()) {
+                if (je.isJsonPrimitive()) out.add(je.getAsString());
+            }
+            return out;
+        }
+        private static <E extends Enum<E>> List<E> readEnumList(JsonObject o, String key, Class<E> enumCls) {
+            if (!o.has(key) || o.get(key).isJsonNull()) return null;
+            JsonElement e = o.get(key);
+            if (!e.isJsonArray()) return null;
+            List<E> out = new ArrayList<>();
+            for (JsonElement je : e.getAsJsonArray()) {
+                if (je.isJsonPrimitive()) {
+                    out.add(Enum.valueOf(enumCls, je.getAsString()));
                 }
             }
+            return out;
         }
-        reader.endObject();
-
-        if(minEnch != null && range != null) {
-            int finalMinEnch = minEnch;
-            int finalRange = range;
-            int finalLvlSpan = lvlSpan == null ? 0 : lvlSpan;
-            MaxEnchantabilityMode finalMaxEnchMode = maxEnchMode;
-            return info -> info.setEnchantabilities(finalMinEnch, finalLvlSpan, finalRange, finalMaxEnchMode);
-        }
-        return null;
     }
 
     private static void parseLegacyEnchantmentsControlConfig(EnchantmentInfo info, String minEnchEval, String maxEnchEval) {
