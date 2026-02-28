cdca63799e5577580b030692e5c2f24cbe2d2772
nischhelm <nischhelm@gmail.com>
2026-02-28 19:12:31 +0100

    v1.0.3
    - fixed config comments being deleted in file on first startup
    - fixed concurrentmodificationexception crash when having modded EnumEnchantments
    - automatically dont target mixin classes when targeting modded enchantments

--- src/main/java/enchantmentcontrol/EnchantmentControl.java
+++ src/main/java/enchantmentcontrol/EnchantmentControl.java
@@ -35,7 +35,7 @@ import java.util.Map;
 )
 public class EnchantmentControl {
     public static final String MODID = "enchantmentcontrol";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String NAME = "EnchantmentControl";
     public static final Logger LOGGER = LogManager.getLogger(EnchantmentControl.NAME);
     public static final String SEP = ",";
