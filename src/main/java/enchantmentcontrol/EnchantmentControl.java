dd6ccd32c63d679b3f578e1f8cf39123c980d917
nischhelm <nischhelm@gmail.com>
2026-04-01 23:43:39 +0200

    v1.0.4.1 hotfix for serverside crash
    feel like this happens too often..

--- src/main/java/enchantmentcontrol/EnchantmentControl.java
+++ src/main/java/enchantmentcontrol/EnchantmentControl.java
@@ -19,7 +19,6 @@ import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -38,7 +37,7 @@ import java.util.Map;
 )
 public class EnchantmentControl {
     public static final String MODID = "enchantmentcontrol";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.4.1";
     public static final String NAME = "EnchantmentControl";
     public static final Logger LOGGER = LogManager.getLogger(EnchantmentControl.NAME);
     public static final String SEP = ",";
