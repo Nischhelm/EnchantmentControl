9336dd05624fc25e43dc9584ea84a261fe08d954
nischhelm <nischhelm@gmail.com>
2026-04-03 22:18:14 +0200

    fix always reroll again
    was still desynced

--- src/main/java/enchantmentcontrol/mixin/vanilla/etable/ContainerEnchantmentMixin_AlwaysReroll.java
+++ src/main/java/enchantmentcontrol/mixin/vanilla/etable/ContainerEnchantmentMixin_AlwaysReroll.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerEnchantment;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,19 +24,19 @@ public abstract class ContainerEnchantmentMixin_AlwaysReroll {
     @Unique private EntityPlayer ec$player;
 
     @Inject(
-            method = "<init>(Lnet/minecraft/entity/player/InventoryPlayer;Lnet/minecraft/world/World;)V",
+            method = "<init>(Lnet/minecraft/entity/player/InventoryPlayer;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V",
             at = @At("TAIL")
     )
-    private void ec_storePlayer(InventoryPlayer playerInv, World worldIn, CallbackInfo ci){
+    private void ec_storePlayer(InventoryPlayer playerInv, World worldIn, BlockPos pos, CallbackInfo ci){
         this.ec$player = playerInv.player;
     }
 
     @Inject(
             method = "onCraftMatrixChanged",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/ContainerEnchantment;detectAndSendChanges()V", shift = At.Shift.AFTER)
+            at = @At(value = "INVOKE", target = "Ljava/util/Random;setSeed(J)V")
     )
     private void ec_alwaysReroll(IInventory inventoryIn, CallbackInfo ci){
-        ((EntityPlayerAccessor)ec$player).setXpSeed(this.rand.nextInt());
-        this.xpSeed = ec$player.getXPSeed();
+        this.xpSeed = this.rand.nextInt();
+        ((EntityPlayerAccessor)ec$player).setXpSeed(this.xpSeed);
     }
 }
