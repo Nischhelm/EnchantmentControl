a09a5451007173ff87e88b9599c9766956acd014
nischhelm <nischhelm@gmail.com>
2026-04-04 15:11:37 +0200

    add string(/enum) check to earlyclassreader

--- src/main/java/enchantmentcontrol/config/EarlyConfigReader.java
+++ src/main/java/enchantmentcontrol/config/EarlyConfigReader.java
@@ -148,6 +148,7 @@ public class EarlyConfigReader {
     private static File configFile = null;
     private static String configBooleanString = null;
     private static String configIntString = null;
+    private static String configStringString = null;
     private static String configDoubleString = null;
     private static Map<String,Boolean> configArrayFilledMap = null;
 
@@ -221,6 +222,34 @@ public class EarlyConfigReader {
             } catch (Exception e) {
                 EnchantmentControl.LOGGER.error(EnchantmentControl.NAME + ": Failed to parse double config {}, {}", name, e);
                 return 0;
+            }
+        }
+        //If config is not generated yet or missing entries, we use the default value that will get written into it right after this
+        else return defaultValue;
+    }
+
+    public static String getString(String name, String defaultValue) {
+        if (configFile == null) configFile = new File("config", EnchantmentControl.MODID + ".cfg");
+
+        if (configStringString == null) {
+            if (configFile.exists() && configFile.isFile()) {
+                try (Stream<String> stream = Files.lines(configFile.toPath())) {
+                    configStringString = stream.filter(s -> s.trim().startsWith("S:") && s.contains("=")).collect(Collectors.joining("$$$"));
+                } catch (Exception ex) {
+                    EnchantmentControl.LOGGER.error("Failed to parse " + EnchantmentControl.NAME + " String config: {}", String.valueOf(ex));
+                }
+            } else configStringString = "";
+        }
+
+        int startIndex = configStringString.indexOf("S:\"" + name + "\"=");
+        if (startIndex != -1) {
+            try {
+                startIndex = configStringString.indexOf("=", startIndex) + 1;
+                int endindex = configStringString.indexOf("$$$", startIndex);
+                return configStringString.substring(startIndex, endindex == -1 ? configStringString.length() : endindex).trim();
+            } catch (Exception e) {
+                EnchantmentControl.LOGGER.error(EnchantmentControl.NAME + ": Failed to parse int config {}, {}", name, e);
+                return defaultValue;
             }
         }
         //If config is not generated yet or missing entries, we use the default value that will get written into it right after this
