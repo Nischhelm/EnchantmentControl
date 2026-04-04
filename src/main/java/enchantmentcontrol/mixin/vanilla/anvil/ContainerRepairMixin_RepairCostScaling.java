c6600ba8168eb6170f2ce566347e065486c4ea71
nischhelm <nischhelm@gmail.com>
2026-04-04 14:43:39 +0200

    default code of updaterepaircost

--- src/main/java/enchantmentcontrol/mixin/vanilla/anvil/ContainerRepairMixin_RepairCostScaling.java
+++ src/main/java/enchantmentcontrol/mixin/vanilla/anvil/ContainerRepairMixin_RepairCostScaling.java
@@ -5,11 +5,18 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import enchantmentcontrol.config.ConfigHandler;
 import enchantmentcontrol.util.AnvilCostUtil;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.ContainerRepair;
+import net.minecraft.item.ItemEnchantedBook;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Map;
 
 @Mixin(ContainerRepair.class)
 public abstract class ContainerRepairMixin_RepairCostScaling {
@@ -60,5 +67,189 @@ public abstract class ContainerRepairMixin_RepairCostScaling {
 
         AnvilCostUtil.setAnvilCount(stackLeftCopy, useCount + 1);
         original.call(stackLeftCopy, repairCost);
+    }
+
+
+    public void updateRepairOutput() {
+        ItemStack itemstack = this.inputSlots.getStackInSlot(0);
+        this.maximumCost = 1;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        if (itemstack.isEmpty()) {
+            this.outputSlot.setInventorySlotContents(0, ItemStack.EMPTY);
+            this.maximumCost = 0;
+        } else {
+            ItemStack itemstack1 = itemstack.copy();
+            ItemStack itemstack2 = this.inputSlots.getStackInSlot(1);
+            Map<Enchantment, Integer> map = EnchantmentHelper.getEnchantments(itemstack1);
+            j = j + itemstack.getRepairCost() + (itemstack2.isEmpty() ? 0 : itemstack2.getRepairCost());
+            this.materialCost = 0;
+            boolean flag = false;
+
+            if (!itemstack2.isEmpty()) {
+                if (!net.minecraftforge.common.ForgeHooks.onAnvilChange(this, itemstack, itemstack2, outputSlot, repairedItemName, j))
+                    return;
+                flag = itemstack2.getItem() == Items.ENCHANTED_BOOK && !ItemEnchantedBook.getEnchantments(itemstack2).isEmpty();
+
+                if (itemstack1.isItemStackDamageable() && itemstack1.getItem().getIsRepairable(itemstack, itemstack2)) {
+                    int l2 = Math.min(itemstack1.getItemDamage(), itemstack1.getMaxDamage() / 4);
+
+                    if (l2 <= 0) {
+                        this.outputSlot.setInventorySlotContents(0, ItemStack.EMPTY);
+                        this.maximumCost = 0;
+                        return;
+                    }
+
+                    int i3;
+
+                    for (i3 = 0; l2 > 0 && i3 < itemstack2.getCount(); ++i3) {
+                        int j3 = itemstack1.getItemDamage() - l2;
+                        itemstack1.setItemDamage(j3);
+                        ++i;
+                        l2 = Math.min(itemstack1.getItemDamage(), itemstack1.getMaxDamage() / 4);
+                    }
+
+                    this.materialCost = i3;
+                } else {
+                    if (!flag && (itemstack1.getItem() != itemstack2.getItem() || !itemstack1.isItemStackDamageable())) {
+                        this.outputSlot.setInventorySlotContents(0, ItemStack.EMPTY);
+                        this.maximumCost = 0;
+                        return;
+                    }
+
+                    if (itemstack1.isItemStackDamageable() && !flag) {
+                        int l = itemstack.getMaxDamage() - itemstack.getItemDamage();
+                        int i1 = itemstack2.getMaxDamage() - itemstack2.getItemDamage();
+                        int j1 = i1 + itemstack1.getMaxDamage() * 12 / 100;
+                        int k1 = l + j1;
+                        int l1 = itemstack1.getMaxDamage() - k1;
+
+                        if (l1 < 0) {
+                            l1 = 0;
+                        }
+
+                        if (l1 < itemstack1.getItemDamage()) // vanilla uses metadata here instead of damage.
+                        {
+                            itemstack1.setItemDamage(l1);
+                            i += 2;
+                        }
+                    }
+
+                    Map<Enchantment, Integer> map1 = EnchantmentHelper.getEnchantments(itemstack2);
+                    boolean flag2 = false;
+                    boolean flag3 = false;
+
+                    for (Enchantment enchantment1 : map1.keySet()) {
+                        if (enchantment1 != null) {
+                            int i2 = map.containsKey(enchantment1) ? ((Integer) map.get(enchantment1)).intValue() : 0;
+                            int j2 = ((Integer) map1.get(enchantment1)).intValue();
+                            j2 = i2 == j2 ? j2 + 1 : Math.max(j2, i2);
+                            boolean flag1 = enchantment1.canApply(itemstack);
+
+                            if (this.player.capabilities.isCreativeMode || itemstack.getItem() == Items.ENCHANTED_BOOK) {
+                                flag1 = true;
+                            }
+
+                            for (Enchantment enchantment : map.keySet()) {
+                                if (enchantment != enchantment1 && !enchantment1.isCompatibleWith(enchantment)) {
+                                    flag1 = false;
+                                    ++i;
+                                }
+                            }
+
+                            if (!flag1) {
+                                flag3 = true;
+                            } else {
+                                flag2 = true;
+
+                                if (j2 > enchantment1.getMaxLevel()) {
+                                    j2 = enchantment1.getMaxLevel();
+                                }
+
+                                map.put(enchantment1, Integer.valueOf(j2));
+                                int k3 = 0;
+
+                                switch (enchantment1.getRarity()) {
+                                    case COMMON:
+                                        k3 = 1;
+                                        break;
+                                    case UNCOMMON:
+                                        k3 = 2;
+                                        break;
+                                    case RARE:
+                                        k3 = 4;
+                                        break;
+                                    case VERY_RARE:
+                                        k3 = 8;
+                                }
+
+                                if (flag) {
+                                    k3 = Math.max(1, k3 / 2);
+                                }
+
+                                i += k3 * j2;
+
+                                if (itemstack.getCount() > 1) {
+                                    i = 40;
+                                }
+                            }
+                        }
+                    }
+
+                    if (flag3 && !flag2) {
+                        this.outputSlot.setInventorySlotContents(0, ItemStack.EMPTY);
+                        this.maximumCost = 0;
+                        return;
+                    }
+                }
+            }
+
+            if (StringUtils.isBlank(this.repairedItemName)) {
+                if (itemstack.hasDisplayName()) {
+                    k = 1;
+                    i += k;
+                    itemstack1.clearCustomName();
+                }
+            } else if (!this.repairedItemName.equals(itemstack.getDisplayName())) {
+                k = 1;
+                i += k;
+                itemstack1.setStackDisplayName(this.repairedItemName);
+            }
+            if (flag && !itemstack1.getItem().isBookEnchantable(itemstack1, itemstack2)) itemstack1 = ItemStack.EMPTY;
+
+            this.maximumCost = j + i;
+
+            if (i <= 0) {
+                itemstack1 = ItemStack.EMPTY;
+            }
+
+            if (k == i && k > 0 && this.maximumCost >= 40) {
+                this.maximumCost = 39;
+            }
+
+            if (this.maximumCost >= 40 && !this.player.capabilities.isCreativeMode) {
+                itemstack1 = ItemStack.EMPTY;
+            }
+
+            if (!itemstack1.isEmpty()) {
+                int k2 = itemstack1.getRepairCost();
+
+                if (!itemstack2.isEmpty() && k2 < itemstack2.getRepairCost()) {
+                    k2 = itemstack2.getRepairCost();
+                }
+
+                if (k != i || k == 0) {
+                    k2 = k2 * 2 + 1;
+                }
+
+                itemstack1.setRepairCost(k2);
+                EnchantmentHelper.setEnchantments(map, itemstack1);
+            }
+
+            this.outputSlot.setInventorySlotContents(0, itemstack1);
+            this.detectAndSendChanges();
+        }
     }
 }
