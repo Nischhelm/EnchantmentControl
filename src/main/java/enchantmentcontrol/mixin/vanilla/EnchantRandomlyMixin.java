2b9a137c5b57cf039d408b9c323b469bb7eb55bf
nischhelm <nischhelm@gmail.com>
2026-08-31 09:40:14 +0200

    fix enchantrandomly crashfix

--- src/main/java/enchantmentcontrol/mixin/vanilla/EnchantRandomlyMixin.java
+++ src/main/java/enchantmentcontrol/mixin/vanilla/EnchantRandomlyMixin.java
@@ -1,36 +1,26 @@
 package enchantmentcontrol.mixin.vanilla;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import enchantmentcontrol.core.EnchantmentDummy;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import enchantmentcontrol.EnchantmentControl;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.world.storage.loot.functions.EnchantRandomly;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import java.util.List;
 
 @Mixin(EnchantRandomly.Serializer.class)
 public abstract class EnchantRandomlyMixin {
-    @ModifyVariable(
+    @Definition(id = "enchantment", local = @Local(type = Enchantment.class))
+    @Expression("enchantment == null")
+    @ModifyExpressionValue(
             method = "deserialize(Lcom/google/gson/JsonObject;Lcom/google/gson/JsonDeserializationContext;[Lnet/minecraft/world/storage/loot/conditions/LootCondition;)Lnet/minecraft/world/storage/loot/functions/EnchantRandomly;",
-            at = @At(value = "LOAD", ordinal = 0),
-            name = "enchantment"
+            at = @At(value = "MIXINEXTRAS:EXPRESSION")
     )
-    private Enchantment ec_dontThrow(Enchantment enchantment) {
-        if(enchantment != null) return enchantment;
-
-        //first injection point -> fail check, don't throw
-        return EnchantmentDummy.dummy;
-    }
-
-    @WrapOperation(
-            method = "deserialize(Lcom/google/gson/JsonObject;Lcom/google/gson/JsonDeserializationContext;[Lnet/minecraft/world/storage/loot/conditions/LootCondition;)Lnet/minecraft/world/storage/loot/functions/EnchantRandomly;",
-            at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z")
-    )
-    private boolean ec_dontAddNull(List<Object> instance, Object e, Operation<Boolean> original){
-        if(e == null) return false; //skip null enchantments
-        return original.call(instance, e);
+    private static boolean ec_skipUnknown(boolean original, @Local(ordinal = 0) String s) {
+        if(original)
+            EnchantmentControl.LOGGER.debug("EnchantmentControl prevented unknown enchantment {} in a loot function from failing the entire loottable", s);
+        return false; //never throw an error
     }
 }
