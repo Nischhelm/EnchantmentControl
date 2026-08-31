6dd3ec0661fadc1ba872198e373fd52ed91f58d2
nischhelm <nischhelm@gmail.com>
2026-08-31 09:47:10 +0200

    v1.1.2.2
    - fixed enchantrandomly fix
    - now depends on fermiumbooter 1.5.0+, not shadowing classgraph anymore

--- src/main/java/enchantmentcontrol/EnchantmentControl.java
+++ src/main/java/enchantmentcontrol/EnchantmentControl.java
@@ -37,13 +37,13 @@ import java.util.Map;
         version = EnchantmentControl.VERSION,
         name = EnchantmentControl.NAME,
         dependencies =
-                "required-after:fermiumbooter@[1.3.2,);" +
+                "required-after:fermiumbooter@[1.5.0,);" +
                 "before:contenttweaker;" +
                 "after:somanyenchantments;"
 )
 public class EnchantmentControl {
     public static final String MODID = "enchantmentcontrol";
-    public static final String VERSION = "1.1.2.1";
+    public static final String VERSION = "1.1.2.2";
     public static final String NAME = "EnchantmentControl";
     public static final Logger LOGGER = LogManager.getLogger(EnchantmentControl.NAME);
     public static final String SEP = ",";
