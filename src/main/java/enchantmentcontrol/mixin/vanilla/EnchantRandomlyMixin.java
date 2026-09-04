7690d95a53fea9e1d066e88e254a1f58ac08a89f
nischhelm <nischhelm@gmail.com>
2026-09-04 23:07:17 +0200

    fix enchantrandomly fix again

--- src/main/java/enchantmentcontrol/mixin/vanilla/EnchantRandomlyMixin.java
+++ src/main/java/enchantmentcontrol/mixin/vanilla/EnchantRandomlyMixin.java
@@ -3,12 +3,16 @@ package enchantmentcontrol.mixin.vanilla;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import enchantmentcontrol.EnchantmentControl;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.world.storage.loot.functions.EnchantRandomly;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.List;
 
 @Mixin(EnchantRandomly.Serializer.class)
 public abstract class EnchantRandomlyMixin {
@@ -22,5 +26,14 @@ public abstract class EnchantRandomlyMixin {
         if(original)
             EnchantmentControl.LOGGER.debug("EnchantmentControl prevented unknown enchantment {} in a loot function from failing the entire loottable", s);
         return false; //never throw an error
+    }
+
+    @WrapOperation(
+            method = "deserialize(Lcom/google/gson/JsonObject;Lcom/google/gson/JsonDeserializationContext;[Lnet/minecraft/world/storage/loot/conditions/LootCondition;)Lnet/minecraft/world/storage/loot/functions/EnchantRandomly;",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z")
+    )
+    private boolean ec_dontAddNullEnchants(List<Enchantment> list, Object enchantment, Operation<Boolean> original){
+        if(enchantment != null) return original.call(list, enchantment);
+        return false;
     }
 }
