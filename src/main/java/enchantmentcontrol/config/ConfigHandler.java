1489304783361af8c284b1e5ccded7e7c0edd0dd
nischhelm <nischhelm@gmail.com>
2026-09-16 19:59:42 +0200

    move configchangedhandler back

--- src/main/java/enchantmentcontrol/config/ConfigHandler.java
+++ src/main/java/enchantmentcontrol/config/ConfigHandler.java
@@ -1,13 +1,21 @@
 package enchantmentcontrol.config;
 
 import enchantmentcontrol.Tags;
+import enchantmentcontrol.bloodanvil.FeatureBloodAnvil;
 import enchantmentcontrol.config.folders.*;
+import enchantmentcontrol.config.provider.BlacklistConfigProvider;
+import enchantmentcontrol.config.provider.IncompatibleConfigProvider;
+import enchantmentcontrol.config.provider.ItemTypeConfigProvider;
 import meldexun.betterconfig.api.BetterConfig;
+import meldexun.betterconfig.api.BetterConfigManager;
 import meldexun.betterconfig.api.LoadEarly;
 import meldexun.betterconfig.api.Order;
 import meldexun.betterconfig.api.tree.IConfigCategory;
 import meldexun.betterconfig.api.tree.IConfigContext;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.versioning.ArtifactVersion;
 
 @BetterConfig(
@@ -74,4 +82,20 @@ public class ConfigHandler {
 	public static <T extends IConfigContext<T>> void migrateConfigs(IConfigCategory<T> category, T context, ArtifactVersion version){
 		ConfigMigrator.handleMigration(category, context, version);
 	}
+
+    @Mod.EventBusSubscriber
+    public static class EventHandler {
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(Tags.MODID)) {
+                BetterConfigManager.sync(Tags.MODID);
+
+                ItemTypeConfigProvider.onResetConfig();
+                BlacklistConfigProvider.onResetConfig();
+                IncompatibleConfigProvider.onResetConfig();
+
+                FeatureBloodAnvil.resetConfigValues();
+            }
+        }
+    }
 }
